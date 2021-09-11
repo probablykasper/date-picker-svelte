f46881a781858f2e0b23137e1071048a722f23ab
Kasper <kasperkh.kh@gmail.com>
2021-09-11 20:20:33 +0200

    Remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
-import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md'],
-  preprocess: [
-    preprocess(),
-    mdsvex({
-      extensions: ['.md'],
-    }),
-  ],
+  preprocess: preprocess(),
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
