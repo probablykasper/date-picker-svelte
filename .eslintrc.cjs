a0c637d6caa0922b530632fc704717285bcbfc31
Kasper <kasperkh.kh@gmail.com>
2021-10-05 23:47:11 +0200

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['svelte3', '@typescript-eslint'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': true,
+    'svelte3/ignore-styles': (attributes) => {
+      // https://github.com/sveltejs/eslint-plugin-svelte3/issues/10
+      return attributes && attributes.lang && attributes.lang !== 'css'
+    },
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+}
