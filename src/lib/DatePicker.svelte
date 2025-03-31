fc5e44cfcd08a5b50fe9fd1a62b3f81bd4c7fb3b
stinger567 <56362342+stinger567@users.noreply.github.com>
2025-03-31 17:31:49 -0400

    fixed-typo

--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -108,7 +108,7 @@
 	} else if (value && value < min) {
 		setValue(toValidDate(value, min))
 	} else if (value && isDisabledDate?.(value)) {
-		setValue(toValidDate(brosweDate, value))
+		setValue(toValidDate(browseDate, value))
 	}
 	function clamp(d: Date, min: Date, max: Date) {
 		if (d > max) {
